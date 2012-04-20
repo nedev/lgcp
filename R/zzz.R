7835c11785ab4543f69f988c0999f705e53ba936
Benjamin M. Taylor <b.taylor1@lancaster.ac.uk>
2012-04-20 00:00:00 +0000

    version 1.01

--- R/zzz.R
+++ R/zzz.R
@@ -9,6 +9,6 @@
 
 .onAttach <- function(libname, pkgname)
 {
-	packageStartupMessage("\n Welcome to 'lgcp': Log-Gaussian Cox Process\n B. Taylor & T.M. Davies & B. Rowlingson & P. Diggle\ntype '?lgcp' for details, or 'vignette(\"lgcp\")' to view the package vignette.", appendLF=T)
+	packageStartupMessage("\n Welcome to 'lgcp': Log-Gaussian Cox Process\n B. Taylor & T.M. Davies & B. Rowlingson & P. Diggle\ntype '?lgcp' for details, or 'vignette(\"lgcp\")' to view the package vignette. Type 'citation(\"lgcp\")' to view the citation for this package.)", appendLF=T)
 }
 
